8a8d12a8ba462b6624278bfb39766028e8d80f36
Irina Stepanova <missirisshoi@github.com>
2022-05-26 15:28:46 +0600

    refactor: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,3 @@
-module.exports = () => require(`./webpack.config.${process.env.NODE_ENV}`);
+module.exports = process.env.NODE_ENV === 'development'
+  ? require('./webpack.config.dev')
+  : require('./webpack.config.prod');
